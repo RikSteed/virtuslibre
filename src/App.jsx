ef841119566561da6eff5a8274627f00cbba8f71
Riccardo <riccardocroce129@outlook.com>
2023-12-31 17:53:34 +0100

    Adding I18N custom implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { StoreContext } from "./provider/StoreProvider";
+
 const App = () => {
-  return <div>App</div>;
+  const { I18N } = useContext(StoreContext);
+  return <div>{I18N.APP.TEST}</div>;
 };
 
 export default App;
---
+++ src/provider/StoreProvider.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react";
+import en from "../assets/i18n/en.json";
+import it from "../assets/i18n/it.json";
+import jp from "../assets/i18n/jp.json";
+import fr from "../assets/i18n/fr.json";
+import pl from "../assets/i18n/pl.json";
+import cn from "../assets/i18n/cn.json";
+import es from "../assets/i18n/es.json";
+import de from "../assets/i18n/de.json";
+
+const languagesTranslation = {
+  en,
+  it,
+  jp,
+  fr,
+  pl,
+  cn,
+  es,
+  de,
+};
+
+export const StoreContext = createContext({
+  I18N: en,
+  changeI18N: () => {},
+});
+
+const StoreProvider = ({ children }) => {
+  const [I18N, setI18N] = useState(
+    languagesTranslation[sessionStorage.getItem("language")] || en
+  );
+  const changeI18N = (language) => {
+    const selectedLanguage = languagesTranslation[language];
+    setI18N(selectedLanguage);
+    sessionStorage.setItem("language", language);
+  };
+  return (
+    <StoreContext.Provider value={{ I18N, changeI18N }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+export default StoreProvider;
